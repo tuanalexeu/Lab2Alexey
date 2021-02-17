f60b1541656222a3ff8e1702d55037da40471d29
AlexeyTyan <alekseytyan45@gmail.com>
2021-02-17 20:31:29 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Lab2Alexey {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            // TODO implement tasks
+        }
+    }
+}
